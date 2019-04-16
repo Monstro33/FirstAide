6bf5281422316969f15acbaecb69fd86cbee1228
Artie Negron <anegroncsu@gmail.com>
2019-04-16 12:46:52 -0400

    Medication's view properly displays user's data.

--- backend/FirstAide/Models/User.cs
+++ backend/FirstAide/Models/User.cs
@@ -10,20 +10,13 @@ namespace FirstAide.Models
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> master
+ 
         //public string  Dob { get; set; }
         //public string Height { get; set; }
         //public string Weight { get; set; }
         //public string BloodPressure { get; set; }
         //public string Allergies { get; set; }
         //public string ChifComplaint { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> master
         //public virtual List <Medication> Medication { get; set; }
     }
 }
