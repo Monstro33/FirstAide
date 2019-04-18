a7ab690392ebcaff62c9d48a3d5f45e8582bd85a
Artie Negron <anegroncsu@gmail.com>
2019-04-17 23:02:22 -0400

    Can display user weight info on dashboard.

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,38 +1,19 @@
 import React, { Component } from "react";
 import "material-dashboard/assets/css/material-dashboard.css";
 import FileCopyOutlined from "@material-ui/icons/FileCopyOutlined";
-import ChartistGraph from "react-chartist";
+
+import WeightChart from "./WeightChart";
 
 class Dashboard extends Component {
   render() {
-    const Data = {
-      labels: ["M", "T", "W", "T", "F", "S", "S"],
-      series: [[12, 9, 7, 8, 5, 1, 15]]
-    };
+    // const userId = this.props.auth.currentUser.userId;
+    const userId = 1;
 
     return (
       <div className="dashboard">
         <div class="container-fluid">
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6">
-              <div class="card card-stats">
-                <div class="card-header card-header-warning card-header-icon">
-                  <div class="card-icon">
-                    <FileCopyOutlined />
-                  </div>
-                  <p class="card-category">Current Weight</p>
-                  <h3 class="card-title">
-                    210
-                    <small>lbs</small>
-                  </h3>
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                    {/* <i class="material-icons text-danger">warning</i>
-                    <a href="#pablo">Get More Space...</a> */}
-                  </div>
-                </div>
-              </div>
               <div class="card card-stats">
                 <div class="card-header card-header-warning card-header-icon">
                   <div class="card-icon">
@@ -48,45 +29,7 @@ class Dashboard extends Component {
                   </div>
                 </div>
               </div>
-              <div class="card card-stats">
-                <div class="card-header card-header-warning card-header-icon">
-                  <div class="card-icon">
-                    <FileCopyOutlined />
-                  </div>
-                  <p class="card-category">Current Weight</p>
-                  <h3 class="card-title">
-                    210
-                    <small>lbs</small>
-                  </h3>
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                    {/* <i class="material-icons text-danger">warning</i>
-                    <a href="#pablo">Get More Space...</a> */}
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header card-chart card-header-warning">
-                  <div  className="ct-chart" id="dailySalesChart">
-                  <ChartistGraph data={Data} type={'Line'} /></div>
-                </div>
-                <div class="card-body">
-                  <h4 class="card-title">Daily Sales</h4>
-                  <p class="card-category">
-                    <span class="text-success">
-                      <i class="fa fa-long-arrow-up" /> 55%{" "}
-                    </span>{" "}
-                    increase in today sales.
-                  </p>
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                    <i class="material-icons">access_time</i> updated 4 minutes
-                    ago
-                  </div>
-                </div>
-              </div>
+              <WeightChart userId={userId} />
             </div>
           </div>
         </div>
---
+++ frontend/src/components/WeightChart.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import ChartistGraph from "react-chartist";
+import FileCopyOutlined from "@material-ui/icons/FileCopyOutlined";
+
+class WeightChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userWeights: [],
+      currentUserId: this.props.userId,
+      currentUserWeights: [],
+      currentWeight: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://localhost:44321/api/userweight")
+      .then(res => res.json())
+      .then(json => this.setState({ userWeights: json }))
+      .then(() => this.setCurrentUserWeight());
+  }
+
+  setCurrentUserWeight() {
+    const userId = this.props.userId;
+    let CurrentWeight = 0;
+    const CurrentUserWeights = this.state.userWeights.map(function(item) {
+      if (item.userId === userId) {
+        CurrentWeight = item.entryFive;
+        return [
+          item.entryOne,
+          item.entryTwo,
+          item.entryThree,
+          item.entryFour,
+          item.entryFive
+        ];
+      }
+    });
+
+    this.setState({ currentUserWeights: CurrentUserWeights, currentWeight: CurrentWeight });
+  }
+
+  render() {
+    // const userId = this.props.userId;
+    // const CurrentUserWeights = this.state.userWeights.map(function(item) {
+    //   if (item.userId === userId) {
+    //     return [
+    //       item.entryOne,
+    //       item.entryTwo,
+    //       item.entryThree,
+    //       item.entryFour,
+    //       item.entryFive
+    //     ];
+    //   }
+    // });
+
+    // const CurrentWeight = CurrentUserWeights[0];
+    // console.log(CurrentWeight);
+
+    const Data = {
+      labels: ["1", "2", "3", "4", "5"],
+      series: this.state.currentUserWeights
+    };
+
+    return (
+      <>
+        <div class="card card-stats">
+          <div class="card-header card-header-warning card-header-icon">
+            <div class="card-icon">
+              <FileCopyOutlined />
+            </div>
+            <p class="card-category">Current Weight</p>
+            <h3 class="card-title">
+              {this.state.currentWeight}
+              <small>lbs</small>
+            </h3>
+          </div>
+          <div class="card-footer">
+            <div class="stats">
+              {/* <i class="material-icons text-danger">warning</i>
+                    <a href="#pablo">Get More Space...</a> */}
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header card-chart card-header-warning">
+            <div className="ct-chart" id="dailySalesChart">
+              <ChartistGraph data={Data} type={"Line"} />
+            </div>
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">Weight Progress</h4>
+            <p class="card-category">Over the last five entries.</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default WeightChart;
